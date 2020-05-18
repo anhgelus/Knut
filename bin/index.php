b7d6da074a42cd040c6f316c422b2bb6910efa37
SurvivalG <52354197+SurvivalG@users.noreply.github.com>
2020-05-18 13:15:48 +0200

    ajout du css

--- bin/index.php
+++ bin/index.php
@@ -7,10 +7,24 @@ $config = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATO
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Knut - Discord with out TOS and open source</title>
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
     <h1>
         Knut
     </h1>
+    <nav>
+        <div class="friends">
+            <img src="https://via.placeholder.com/150" alt="Friends">
+        </div>
+        <div class="main server-list">
+            <img src="https://via.placeholder.com/150" alt="Serv 1">
+            <img src="https://via.placeholder.com/150" alt="Serv 2">
+            <img src="https://via.placeholder.com/150" alt="Serv 3">
+        </div>
+    </nav>
+    <div class="main">
+        <p>this is a test</p>
+    </div>
 </body>
 </html>
